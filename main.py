f81d4e0242033cf295b70e7f8583dbb0f08f56cb
Milan Bojovic <milanbojovic@gmail.com>
2018-07-02 07:45:13 +0200

    added - label encoding to int for feature selection added - feature selection using RFE Recursive Feature Elimination
    Signed-off-by: Milan Bojovic <milanbojovic@gmail.com>

--- main.py
+++ main.py
@@ -1,38 +1,31 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
 
+#Set desired print width
+desired_width = 500
+pd.set_option('display.width', desired_width)
+np.set_printoptions(linewidth=desired_width)
+
+pd.options.mode.chained_assignment = None
+
+'''
 raw_data = pd.read_csv('input/data.csv')
-
-print(raw_data['shot_made_flag'].head())
 
 #Extract undefined values:
 prediction_dataset = raw_data[raw_data['shot_made_flag'].isnull()]
 
-# Training dataset
+# Testing dataset
 test_data = raw_data[raw_data['shot_made_flag'].notnull()]
+#test_data.shot_made_flag = test_data['shot_made_flag'].astype(np.int64)
 
 #Clean non relevant columns
 test_data = test_data.drop(['game_event_id', 'game_id', 'team_id', 'team_name', 'shot_id', 'game_date', 'matchup'], axis=1)
-
-#Label Encoding binary using get_dummies
-#test_data = pd.get_dummies(test_data,
-#                            columns=["action_type", "combined_shot_type", "period",
-#                                     "season", "shot_type", "shot_zone_area",
-#                                     "shot_zone_basic", "shot_zone_range", "opponent"])
-
-print(test_data.head())
-
-#lb_action_type = LabelEncoder()
-#test_data["action_type_code"] = lb_action_type.fit_transform(test_data["action_type"])
-#print(test_data[["action_type", "action_type_code"]].head(11))
-
-#lb_style = LabelBinarizer()
-#lb_results = lb_style.fit_transform(test_data["action_type"])
-#ggg = pd.DataFrame(lb_results, columns=lb_style.classes_).head()
-#print(ggg)
-
-
+'''
 '''
 #Encode categorical data to integer
 test_data.action_type=test_data.loc[:,'action_type'].apply(lambda x :  np.where(x == test_data.action_type.unique())[0][0])
@@ -52,11 +45,52 @@ test_data.shot_zone_area=test_data.loc[:,'shot_zone_area'].apply(lambda x :  np.
 test_data.shot_zone_basic=test_data.loc[:,'shot_zone_basic'].apply(lambda x :  np.where(x == test_data.shot_zone_basic.unique())[0][0])
 test_data.shot_zone_range=test_data.loc[:,'shot_zone_range'].apply(lambda x :  np.where(x == test_data.shot_zone_range.unique())[0][0])
 test_data.opponent=test_data.loc[:,'opponent'].apply(lambda x :  np.where(x == test_data.opponent.unique())[0][0])
+
+# Feature selection
+X = test_data.drop('shot_made_flag', axis=1)
+Y = test_data['shot_made_flag']
+
+model = LogisticRegression()
+rfe = RFE(model, 10)
+fit = rfe.fit(X, Y)
+
+print(fit.n_features_)
+print(fit.ranking_)
+print(fit.support_)
 '''
 
+test_data  = pd.read_csv('input/test_data_label_encoded.csv')
 
-print('Column count after cleanup: ' + str(len(test_data.columns)))
-print('Row count after cleanup: ' + str(len(test_data.index)))
+#After analysis following columns were removed: lat, lon, loc_x, loc_y, seconds_remaining, opponent
+test_data = test_data.drop(['lat', 'loc_x', 'loc_y', 'lon', 'seconds_remaining', 'opponent'], axis=1)
+
+#Label Encoding binary using get_dummies
+test_data = pd.get_dummies(test_data,
+                           columns=["action_type", "combined_shot_type", "period",
+                                     "season", "shot_type", "shot_zone_area",
+                                     "shot_zone_basic", "shot_zone_range"])
+
+#train, test = train_test_split(test_data, test_size=0.2)
+
+#print(len(train))
+#print(len(test))
+
+#print('Shape train' + str(train.shape))
+#print('Shape test' + str(test.shape))
+
+#print('TEST 1 2 3 4 5 6 7 8 9 10')
+#print(test_data.dtypes)
+
+
+# prepare cross validation
+#kfold = KFold(10)
+
+# enumerate splits
+#for train, eval in kfold.split(test_data):
+#    print('train indexes: %s, eval indexes: %s' % (test_data.iloc[train,:], test_data.iloc[eval, :]))
+
+#print('Column count after cleanup: ' + str(len(test_data.columns)))
+#print('Row count after cleanup: ' + str(len(test_data.index)))
 
 #print('Test data prepared:')
 #print(test_data)
